597aa4fb12afec15663fb44a851dd62c4600377f
Ipsita Mondal <Ipsitaamondal@gmail.com>
2026-07-29 16:54:24 +0530

    Time: 8 ms (7.82%) | Memory: 21.2 MB (7.39%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    void dfs(unordered_map<int,vector<int>>&adj,int u,vector<bool>&visited){
-        visited[u]=true;
-
-        for(int &v:adj[u]){
-            if(!visited[v]){
-                dfs(adj,v,visited);
-    
-            }
+void dfs( unordered_map<int,vector<int>>&adj,int u,vector<bool>&visited){
+    visited[u]=true;
+    for(int &v:adj[u]){
+        if(!visited[v]){
+            dfs(adj,v,visited);
         }
     }
+
+}
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-         vector<bool>visited(n,false);
+        vector<bool>visited(n,false);
         unordered_map<int,vector<int>>adj;
         for(int u=0;u<n;u++){
             for(int v=0;v<n;v++){
-               if(isConnected[u][v]==1){
+                if(isConnected[u][v]==1){
                     adj[u].push_back(v);
+                    adj[v].push_back(u);
                 }
             }
         }
         int count=0;
-
         for(int i=0;i<n;i++){
-
             if(!visited[i]){
-
-                count++;      // new province found
-
-                dfs(adj,i,visited);
+             dfs(adj,i,visited);
+            count++;
             }
-        }
-
-        return count;
-       
+        } 
+       return count;
     }
 };

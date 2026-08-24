d2e8609cf1a85dee2f6ba028bca851142d773329
Ipsita Mondal <Ipsitaamondal@gmail.com>
2026-08-24 10:21:30 +0530

    Time: 4 ms (20.68%) | Memory: 63 MB (11.12%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -10,7 +10,7 @@ public:
             int area=h*(j-i);
             maxArea=max(area,maxArea);
 
-           (height[i]<height[j])?i++:j--;
+           (height[i]<height[j])?i++:j--;//ternary operator 
         }
         return  maxArea;
     }

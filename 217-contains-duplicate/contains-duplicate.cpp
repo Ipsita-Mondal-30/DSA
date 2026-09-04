3fbf8d9d52282d60bbc36c6b4f69aff9bfe6dc04
Ipsita Mondal <Ipsitaamondal@gmail.com>
2026-09-04 22:48:29 +0530

    Time: 72 ms (53.75%) | Memory: 111.2 MB (62.88%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,22 +2,13 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         int n=nums.size();
-       unordered_map<int,int>mpp;
-       for(int i:nums){
-        if(mpp.count(i)){
-            return true;
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            if(mpp.count(nums[i])){
+                return true;
+            }
+            mpp[nums[i]]++;
         }
-        mpp[i]++;
-       }
-    //    for(int i=0;i<n;i++){
-    //     if(mpp.count(nums[i])){
-    //         return true;
-    //     }
-    //         // if(mpp.begin(nums[i])!=mpp.end()){
-    //         //     return true;
-    //         // }
-    //         mpp[nums[i]]++;
-    //    } 
-    return false;
+        return false;
     }
 };
